b06f4edc60a06289b60c67e1b2819acecdb19e13
Subhan Rizki <113665663+subhanrizki@users.noreply.github.com>
2023-03-15 00:12:35 +0700

    Membuat Profile

---
+++ Pertemuan1/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return <View style={{flex:1}}>
+    <StatusBar barStyle={'dark-content'} backgroundColor="#FAFAFA" />
+  </View style={{flex: 0,5, backgroundColor: '#212121'}}></View> 
+  </View style={{flex: 1, backgroundColor: '#212121'}}></View> 
+}
