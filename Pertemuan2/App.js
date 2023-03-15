282d5e78389ed6a76b479b11885514f307edecd0
Subhan Rizki <113665663+subhanrizki@users.noreply.github.com>
2023-03-15 11:40:38 +0700

    Pertemuan 2

---
+++ Pertemuan2/App.js
@@ -0,0 +1,12 @@
+import Car from "./components/Car";
+import Motorcycle from "./components/Motorcycle";
+import React, { Component } from 'react'
+
+export default function App () {
+  return (
+    <>
+    <Car />
+    <Motorcycle/>
+    </>
+  );
+}
